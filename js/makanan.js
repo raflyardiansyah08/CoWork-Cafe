c6a72900aac9f64b5c26569b28eb38d6289b4451
raflyardiansyah08 <ardisrigala18@gmail.com>
2024-12-15 16:02:31 +0700

    baru diedit

--- js/makanan.js
+++ js/makanan.js
@@ -21,7 +21,7 @@ const menuItems = [
     nama: 'Pudding Coklat',
     kategori: 'dessert',
     harga: 'Rp 15.000',
-    deskripsi: 'Pudding coklat lembut dengan saus vanilla',
+    deskripsi: 'Pudding coklat lembut dengan susu vanilla',
     gambar: 'https://i.pinimg.com/736x/fd/48/f2/fd48f2048d577027d70ce232a5c62957.jpg',
   },
   {
@@ -39,6 +39,30 @@ const menuItems = [
     harga: 'Rp 10.000',
     deskripsi: 'Kue kering seperti chocolate chip, oatmeal, yang sering disajikan dalam potongan kecil.',
     gambar: 'https://i.pinimg.com/736x/a2/54/ab/a254ab3176f62d952a39db1c7a2a6a2b.jpg',
+  },
+  {
+    id: 6,
+    nama: 'Brownies Coklat',
+    kategori: 'dessert',
+    harga: 'Rp 20.000',
+    deskripsi: 'Kue Coklat padat dan lembut, Dan dalamnya berisi coklat',
+    gambar: 'https://i.pinimg.com/736x/d1/71/38/d17138bad419886a76a159111dda46c1.jpg',
+  },
+  {
+    id: 7,
+    nama: 'Pasta',
+    kategori: 'makanan',
+    harga: 'Rp 30.000',
+    deskripsi: 'Pasta dengan atasnya berisi daging dan sayuran tipis',
+    gambar: 'https://i.pinimg.com/736x/26/53/e8/2653e8eb9eab00d0ecc2fb9a40581795.jpg',
+  },
+  {
+    id: 8,
+    nama: 'Tart atau Pie',
+    kategori: 'dessert',
+    harga: 'Rp 25.000',
+    deskripsi: 'Tart buah atau pie dengan isian strawberry',
+    gambar: 'https://i.pinimg.com/474x/e3/aa/99/e3aa992d25925b773d1961e02d567734.jpg',
   },
 ];
 
