19b703990126d7d1a6172a6ad52bc58e2be7ffa7
raflyardiansyah08 <ardisrigala18@gmail.com>
2024-12-17 14:15:26 +0700

    file baru edit

--- js/main.js
+++ js/main.js
@@ -3,40 +3,49 @@ const menuToggle = document.querySelector('.menu-toggle');
 const navLinks = document.querySelector('.nav-links');
 
 menuToggle.addEventListener('click', () => {
-    navLinks.classList.toggle('active');
+  navLinks.classList.toggle('active');
 });
 
 // Smooth Scrolling
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth',
     });
+  });
 });
 
 // Form Submission
-const contactForm = document.querySelector('.contact-form');
-contactForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-    alert('Thank you for your message! We will get back to you soon.');
-    contactForm.reset();
+document.getElementById('contactForm').addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  // Ambil Data Formulir
+  const name = document.getElementById('name').value;
+  const email = document.getElementById('email').value;
+  const message = document.getElementById('message').value;
+
+  // Kirim Pesan Ke WhatsApp
+  const whatsappMessage = `Halo, Nama Saya ${name}.\nEmail: ${email}\nMessage: ${message}`;
+  const whatsappURL = `https://wa.me/628819624363?text=${encodeURIComponent(whatsappMessage)}`;
+
+  // Arahkan ke WhatsApp
+  window.open(whatsappURL, '_blank');
+  window.location.href = '/';
 });
 
 // Pricing Toggle Animation
 const priceCards = document.querySelectorAll('.price-card');
-priceCards.forEach(card => {
-    card.addEventListener('mouseover', () => {
-        if (!card.classList.contains('featured')) {
-            card.style.transform = 'translateY(-10px)';
-        }
-    });
+priceCards.forEach((card) => {
+  card.addEventListener('mouseover', () => {
+    if (!card.classList.contains('featured')) {
+      card.style.transform = 'translateY(-10px)';
+    }
+  });
 
-    card.addEventListener('mouseout', () => {
-        if (!card.classList.contains('featured')) {
-            card.style.transform = 'translateY(0)';
-        }
-    });
+  card.addEventListener('mouseout', () => {
+    if (!card.classList.contains('featured')) {
+      card.style.transform = 'translateY(0)';
+    }
+  });
 });
-
